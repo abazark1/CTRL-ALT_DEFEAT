618c747feeb8ed576ea24309970ebe6a63b21c93
Aliia Bazarkulova <aliia.bazarkulova@ericsson.com>
2024-03-13 16:44:23 +0100

    Project initialized, class Curse created

---
+++ game-logic/Curse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author ebazali
+ */
+public class Curse {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+}
