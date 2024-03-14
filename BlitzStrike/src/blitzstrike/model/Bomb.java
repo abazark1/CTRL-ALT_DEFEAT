b86c40c0241fd7054cf54a4a7e73a1c25763eea6
Chyong Artur <xog5d9@inf.elte.hu>
2024-03-14 10:43:18 +0100

    create class Bomb

---
+++ BlitzStrike/src/blitzstrike/model/Bomb.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blitzstrike.model;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author artur
+ */
+public class Bomb {
+    private LocalTime startingTime;
+    private Position position;
+    // private Player owner;
+    
+    public void explode(){
+        System.out.println("I exploded");
+    }
+    
+    public void handleExplosion(){
+        System.out.println("I am handling explosion");
+    }
+    
+}
