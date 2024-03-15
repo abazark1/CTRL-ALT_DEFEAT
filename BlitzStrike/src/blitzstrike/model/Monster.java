55f127eae626467a4e396c22f8e7a98278febdf4
ASEL BEKBOLOT KYZY <aselhobi02@gmail.com>
2024-03-15 09:38:02 +0100

    Monster classes added

---
+++ BlitzStrike/src/blitzstrike/model/Monster.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blitzstrike.model;
+
+/**
+ *
+ * @author aselh
+ */
+import java.util.ArrayList;
+public class Monster {
+    private double speed;
+    private boolean ableToGoThroughWalls;
+    private boolean alive;
+    private Position position;
+    
+    
+    public void move(Direction direction){
+        
+    }
+    public void getExploded(){
+        
+    }
+    public void attack(Player p){
+        
+    }
+    public Position isPlayerClose(Position p){
+        return;
+    }
+    
+    public boolean isValidPosition(Position p){
+        return;
+    }
+}   
+class BasicMonster extends Monster{
+    @Override
+    public void move(Direction d){
+
+    }
+
+}
+
+class Monster2 extends Monster{
+    @Override
+    public void move(Direction d){
+
+    }
+
+}
+class Monster3 extends Monster{
+    @Override
+    public void move(Direction d){
+
+    }
+
+    private Direction checkClosestPlayer(ArrayList<Cell> space){
+
+        return;
+    }
+}
+
+class Monster4 extends Monster{
+    @Override
+    public void move(Direction d){
+
+    }
+
+}      
+
+
