4ede82b615fecc2c8c11a39c4cb138ef1289a7a7
emedash <emedash@192.168.0.38>
2024-04-02 14:18:20 +0200

    MainWindow changes added

--- BlitzStrike/src/blitzstrike/view/MainWindow.java
+++ BlitzStrike/src/blitzstrike/view/MainWindow.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -35,7 +36,6 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-import javax.swing.Timer;
 import res.ResourceLoader;
 
 /**
@@ -51,9 +51,6 @@ public class MainWindow extends JFrame {
     private Player player2;
 
     private JFrame frame;
-    
-    private Timer monsterMoveTimer;
-    private static final int MONSTER_MOVE = 2000;
 
     public MainWindow() throws IOException {
 
@@ -114,14 +111,13 @@ public class MainWindow extends JFrame {
 
             };
 
-        quitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                confirmExit();
-            }
-        });
-        
-        startMoveMonsterTimer();
+            //mMenu.setLayout(new BorderLayout());
+            mMenu.setBackground(Color.BLACK);
+            mMenu.setLayout(new BoxLayout(mMenu, BoxLayout.Y_AXIS));
+            mMenu.add(Box.createVerticalGlue());
+            mMenu.add(Box.createVerticalGlue());
+            mMenu.add(Box.createVerticalGlue());
+            mMenu.add(Box.createVerticalGlue());
 
             JButton playButton = createButton("PLAY");
             JButton continueButton = createButton("CONTINUE");
@@ -164,27 +160,6 @@ public class MainWindow extends JFrame {
                     confirmExit();
                 }
             });
-    }
-    
-    
-    //for continuous movement of monsters
-    private void startMoveMonsterTimer() {
-        monsterMoveTimer = new Timer(MONSTER_MOVE, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Move monsters
-                moveMonsters();
-            }
-        });
-        monsterMoveTimer.start();
-    }
-    
-    private void moveMonsters() {
-        if (game != null) {
-            game.moveMonsters();
-            // Repaint the view to update the monster positions
-            view.repaint();
-        }
     }
 
     private JButton createButton(String label) {
@@ -386,3 +361,4 @@ public class MainWindow extends JFrame {
         gameSetupDialog.setVisible(true);
     }
 }
+
