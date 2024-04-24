49e6ab21838bdad2358f72354022f1cf93b5cae3
Chyong Artur <xog5d9@inf.elte.hu>
2024-04-24 23:28:13 +0200

    Initialize test file

---
+++ BlitzStrike/test/blitzstrike/model/GameTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package blitzstrike.model;
+
+import blitzstrike.model.monsters.Monster;
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    public GameTest() {
+    }
+
+}
