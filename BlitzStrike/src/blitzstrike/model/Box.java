60281c427024982376b1fec5a803afad65d93fd1
Chyong Artur <xog5d9@inf.elte.hu>
2024-05-03 16:40:13 +0200

    Fix boxes drop new effect after further explosions

--- BlitzStrike/src/blitzstrike/model/Box.java
+++ BlitzStrike/src/blitzstrike/model/Box.java
@@ -11,37 +11,39 @@ import blitzstrike.model.effects.Effect;
  * @author medina
  */
 public class Box extends Cell {
-    
+
     private Effect effect;
-    
-    public Box(Position position){
+
+    public Box(Position position) {
         super(position);
         this.destroyable = true;
         this.mineable = false;
         this.walkable = false;
     }
-    
-    public void getDestroyed(){
-        this.destroyed = true;
-        this.walkable = true;
-        this.destroyable = false;
-        this.mineable = true;
-        this.effect = Effect.getRandomEffect();
-        System.out.println("I'm destroyed");
+
+    public void getDestroyed() {
+        if (!this.destroyed) {
+            System.out.println("I'm destroyed");
+            this.destroyed = true;
+            this.walkable = true;
+            this.destroyable = false;
+            this.mineable = true;
+            this.effect = Effect.getRandomEffect();
+        }
     }
-    
+
     public Effect getEffect() {
         return this.effect;
     }
-    
-    public boolean isDestroyed(){
+
+    public boolean isDestroyed() {
         return this.destroyed;
     }
-    
-    public boolean isDestoyable(){
+
+    public boolean isDestoyable() {
         return this.destroyable;
     }
-   
+
     public Position getPosition() {
         return this.position;
     }
